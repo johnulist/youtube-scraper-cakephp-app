caddb3a8f0a4e2e7427eb6f72370b8f82bd8a1c7
Andras Kende <andras@kende.com>
2011-11-17 04:00:02 -0600

    Update Controller/AppController.php

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -1,7 +1,7 @@
 <?php
 class AppController extends Controller {
 
-	public $helpers = array('Html', 'Form', 'Session', 'Text', 'Js' => array('Jquery'));
+	public $helpers = array('Html', 'Form', 'Session', 'Text');
 	public $components = array('Session', 'Auth');
 
 	public function beforeFilter() {
